fd2d301e6c592797632ec677be7db4f4777ce96e
mr-flintstone <39106277+mr-flintstone@users.noreply.github.com>
2019-02-02 10:42:42 -0800

    Add files via upload

---
+++ src/js/title2.js
@@ -0,0 +1,15 @@
+title_maker();
+function title_maker() {
+
+  var github = document.createElement("p");
+  github.innerHTML = "<a href=\"https://github.com/mr-flintstone/light-node-amoveo/blob/master/README.md\">Instructions</a>";
+  document.body.appendChild(github);
+
+
+    var github = document.createElement("p");
+    github.innerHTML = "<a href=\"https://github.com/zack-bitcoin/amoveo/blob/master/README.md\">Learn more about Amoveo</a>";
+    document.body.appendChild(github);
+
+
+
+};
